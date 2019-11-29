193cc239c8f0f7617e618ffca1f2a02517d2314b
Monkey-D-Code <theartofdeception08@gmail.com>
2019-11-29 18:23:36 +0530

    pagination fix

--- Pickinghub/settings.py
+++ Pickinghub/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Pickinghub.wsgi.application'
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    # 'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE' : 100,
 }
 
